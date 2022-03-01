aa87a073d4669ff1ebc1afe90fb9ea9369a532fe
chibuzc <chibuzc@yahoo.co.uk>
2022-03-01 23:58:29 +0100

    setup ingres-nginx

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("current user");
+});
+
 app.listen(4000, () => {
   console.log("listening on port 4000!!");
 });
